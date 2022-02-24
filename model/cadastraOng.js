359cb5581547d8f25c354429e7bdd717459ab354
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2022-02-23 22:34:22 -0300

    final

--- model/cadastraOng.js
+++ model/cadastraOng.js
@@ -1,23 +1,35 @@
+
+const ongController = require('../controller/ongController')
 const db = require('../database/database')
+
 
 class CadastrarOng{
 
 
-    cadastrarOng(valores, res){
-        const sql = `INSERT INTO endereco (local) VALUES (${valores.endereco})`
+    cadastrar(newOng, res){
+        
+        var colunas = Object.keys(newOng).join(',')
+        var valores = Object.values(newOng)
+                                .map((v) => `"${v}"`)
+                                .join(',')
+
+        var sql = `INSERT INTO ong(${colunas}) VALUES (${valores})`
+
+        console.log(sql)
 
         db.run(sql,(erro, resposta)=>{
             if(erro){
-                console.log("endereco ERR")
-                res.status(400).send(erro)
+              var infoErr = {
+                  msg: `ESTE CNPJ JÁ ESTÁ CADASTRADO NO SITE`
+              }
+                res.status(400).json(erro)
             }else{
-                var chaves = {
-                    cpf: valores.cpf, 
-                }
+
+                res.redirect('http://localhost:8080/index.html')
             }
         })
 
     }
-
-
+    
 }
+module.exports = new CadastrarOng
--- model/cadastraUsuario.js
+++ model/cadastraUsuario.js
@@ -1,6 +1,7 @@
 const db = require('../database/database')
 
 class CadastraUsuario{
+    
     cadastrar(newUser, res){
 
         //POST novo usuário
@@ -21,77 +22,12 @@ class CadastraUsuario{
               var infoErr = {
                   msg: `ESTE CPF JÁ ESTÁ CADASTRADO NO SITE`
               }
-                res.status(400).json(infoErr)
+                res.status(400).json(infoErr.msg)
             }else{
-                var info = {
-                    msg: `USUÁRIO CADASTRADO COM SUCESSO.`
-                }
-               res.status(200).json(info);
+                res.redirect('http://localhost:8080/index.html');
             }
         })
 
-    }
-
-
-
-   
-
-    
-
-    
-    retornaTodos(res){
-        const sql = 'SELECT * FROM usuario'
-
-        db.all(sql, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro)
-            } else{
-                res.status(200).json(resultados)
-            }
-        })
-    }
-
-    obterPorId(id, res){
-        const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
-
-        db.get(sql, (erro, resultado) => {
-            if(erro){
-                res.status(400).json(erro)
-            }else{
-                
-                res.status(200).json(resultado)
-            }
-        })
-    }
-
-    alteraAtendimento(id, status, res){
-        
-        var value = `"${Object.values(status)}"`
-  
-        console.log(value)
-        const sql = `UPDATE Atendimentos SET status = ${value} WHERE id = ${id}`
-        console.log(sql)
-        
-        db.exec(sql, (erro, resultado) => {
-            if(erro){
-                res.status(400).json(erro)
-            }else{
-                res.status(200).json(resultado)
-            }
-        })
-    }
-
-
-    deletaAtendimento(id, res){
-        const sql = `DELETE FROM Atendimentos WHERE id = ${id}`
-
-        db.run(sql, (erro, resultado) => {
-            if(erro){
-                res.status(400).json(erro)
-            }else{
-                res.status(400).json(resultado)
-            }
-        })
     }
 }
 
